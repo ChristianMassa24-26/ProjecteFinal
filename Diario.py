9e0ad61356731648388f288bcd529a829e89dffb
ChristianMassa24-26 <christian.masa@iaballester.cat>
2025-04-01 09:25:36 +0200

    Update Diario.py

--- Diario.py
+++ Diario.py
@@ -1,4 +1,4 @@
-# Notas dia 1 Proyecto - Christian Massa
+# Notas dia 1 (Viernes 28 Marzo) Proyecto - Christian Massa
 
 
 # Proyecto final de curso de Python / Pygame.
@@ -6,3 +6,8 @@
 # Videojuego tipo RPG (Algo como los Pokémon antiguos) 
 
 # Con Didac Perales
+
+# Notas dia 2 (Lunes 31 de marzo) 
+
+# Didac ha terminado de hacer PORFIN el sprite del personaje del jugador. Pero solo el principal, el que se ve de frente.
+# He hecho los primeros tiles (texturas/fragmentos de mundo), son de arena, los cuales ahora estoy haciendo modificaciones como por ejemplo, colocar tierra en los bordes.
