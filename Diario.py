d029ae06f1d20f95e5c0e7d732fac0da775a906c
ChristianMassa24-26 <christian.masa@iaballester.cat>
2025-04-02 10:05:57 +0200

    Update Diario.py

--- Diario.py
+++ Diario.py
@@ -7,7 +7,13 @@
 
 # Con Didac Perales
 
-# Notas dia 2 (Lunes 31 de marzo) 
+# Notas dia 2 (Lunes 31 de Marzo) 
 
 # Didac ha terminado de hacer PORFIN el sprite del personaje del jugador. Pero solo el principal, el que se ve de frente.
 # He hecho los primeros tiles (texturas/fragmentos de mundo), son de arena, los cuales ahora estoy haciendo modificaciones como por ejemplo, colocar tierra en los bordes.
+
+# Notas dia 3 (Miercoles 2 de Abril) 
+# Código inicial completado, si, he usado una inteligencia artificial, pero ahora almenos tenemos una idea de como hacer el juego. Planeo usar algunos tutoriales
+# junto a la inteligencia artificial para aprender a hacer el juego por mi propia mano.
+
+# Voy a colocar como será el juego, o almenos como pensamos que será.
